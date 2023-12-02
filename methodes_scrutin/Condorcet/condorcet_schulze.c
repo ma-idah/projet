6f52622515f499a18aa20dc2daea448a73934b01
aymericfrcs <aymeric.frcs@gmail.com>
2023-12-02 17:51:44 +0100

    Condorcet fonctionnel manque juste une fonction général pour renvoyer le nom du vainqueur

--- methodes_scrutin/Condorcet/condorcet_schulze.c
+++ methodes_scrutin/Condorcet/condorcet_schulze.c
@@ -58,6 +58,7 @@ void deleteDualMatrix(DualMatrix *mat) {
 int SingleDualResult(const CsvMatrix *mat, int index1, int index2, int tabResult[2]) { 
     int score = 0;
     for (int i=1; i<mat->nbRows; i++) {
+        //on compare pour chaque votant le classement des deux candidats
         if (atoi(mat->matrix[i][index1]) < atoi(mat->matrix[i][index2])) {
             score++;
             tabResult[0]++;
@@ -77,18 +78,19 @@ void fillDualMatrix(const CsvMatrix *mat, struct s_DualMatrix *dualMat) {
     int *tabResult = malloc(sizeof(int)*2);
     int score;
 
-    for (int i=0; i<nbCandidates; i++) {
-        for (int j=i; j<nbCandidates; j++) {
+    for (int i=0; i<nbCandidates; i++) { //pour chaque candidat
+        for (int j=i; j<nbCandidates; j++) { //pour chaque adversaire dont on a pas encore simulé le duel
             if (i != j) {
-                printf("Duel entre %s et %s:\n", mat->matrix[0][i+4], mat->matrix[0][j+4]);
+                //printf("Duel entre %s et %s:\n", mat->matrix[0][i+4], mat->matrix[0][j+4]);
                 tabResult[0] = tabResult[1] = 0;
                 score = SingleDualResult(mat, i+4, j+4, tabResult);
-                printf("%d contre %d, score : %d\n", tabResult[0], tabResult[1], score);
-                dualMat->matrix[i][j] = tabResult[0];
+                //printf("%d contre %d, score : %d\n", tabResult[0], tabResult[1], score);
+                //on remplie les cases de la matrice de duel avec les scores des deux adversaires
+                dualMat->matrix[i][j] = tabResult[0]; 
                 dualMat->matrix[j][i] = tabResult[1];
-                if (tabResult[0] > tabResult[1]) {
+                /*if (tabResult[0] > tabResult[1]) {
                     printf("candidat 1 a gangné ce duel!\n\n");
-                }
+                }*/
             } else {
                 dualMat->matrix[i][j] = 0; //ces cases ne seront pas consultées de toute manière, je vais peut être simplement supprimer cette ligne 
             }
@@ -96,8 +98,33 @@ void fillDualMatrix(const CsvMatrix *mat, struct s_DualMatrix *dualMat) {
     }
 }
 
+//renvoie l'indice du gagnant, et -1 si pas de gagnant
+int DualMatrixWinner(DualMatrix *dualMat) {
+    int victoires;
+    for (int i=0; i<dualMat->nbRows; i++) { //parcours des lignes
+        victoires = 0;
+        for (int j=0; j<dualMat->nbCols; j++) { //parcours des colonnes
+            if (i != j) {
+                if (dualMat->matrix[i][j] < dualMat->matrix[j][i]) { //i a perdu contre j
+                    break;
+                } else {
+                    victoires++;
+                }
+            }
+        }
+        if (victoires == dualMat->nbCols-1) { // i a gagné tous ses duels
+            return i;
+        }
+    }
+    return -1;
+}
+
 void printDualMatrix(const DualMatrix *mat) {
-    printf("oui\n");
+    for (int k=0; k<mat->nbCols; k++) {
+            printf("----------------"); 
+        }
+        printf("\n");
+
     for (int i=0; i<mat->nbRows; i++) {
         for (int j=0; j<mat->nbCols; j++) {
             printf("|\t%d\t", mat->matrix[i][j]);
@@ -111,6 +138,7 @@ void printDualMatrix(const DualMatrix *mat) {
     }
 }
 
+//TODO VERIFIER QUE CA MARCHE BIEN
 int main(int argc, char const *argv[])
 {
     char file[MAX_STRING_SIZE];
@@ -119,11 +147,13 @@ int main(int argc, char const *argv[])
     CsvRowsColsCounter(&rows, &cols, file);
     CsvMatrix *mat = initCsvMatrix(rows, cols);
     FillMatrix(mat, file);
-    printMatrix(mat);
 
     DualMatrix *dualMat = initDualMatrix(cols-4, cols-4);
     fillDualMatrix(mat, dualMat);
     printDualMatrix(dualMat);
+
+    int vainqueur = DualMatrixWinner(dualMat);
+    printf("vainqueur : n°%d", vainqueur);
 
     //printf("\n\n %d\n", dualMat->matrix[1][0]);
 
